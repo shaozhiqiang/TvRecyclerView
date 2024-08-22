5225604172bfb1b4cc9a21ea5ef6d415198b5c7a
szq <465149562@qq.com>
2024-08-22 19:59:06 +0800

    添加Tab

---
+++ example/src/main/java/com/owen/tvrecyclerview/example/DimenUtil.java
@@ -0,0 +1,16 @@
+package com.owen.tvrecyclerview.example;
+
+import android.content.Context;
+
+public class DimenUtil {
+
+    public static int px2dip(Context context, float pxValue) {
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int)(pxValue / scale + 0.5F);
+    }
+
+    public static int dip2px(Context context, float dipValue) {
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int)(dipValue * scale + 0.5F);
+    }
+}
